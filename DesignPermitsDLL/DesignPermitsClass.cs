90bfbb9a7719b783140bffe9efce642e68fd69d5
Terry Holmes <tchaser3@outlook.com>
2019-06-14 15:35:14 -0400

    Add new routines

--- DesignPermitsDLL/DesignPermitsClass.cs
+++ DesignPermitsDLL/DesignPermitsClass.cs
@@ -45,6 +45,235 @@ namespace DesignPermitsDLL
         FindPermitsByLocationDataSet aFindPermitsByLocationDataSet;
         FindPermitsByLocationDataSetTableAdapters.FindPermitsByLocationTableAdapter aFindPermitsByLocationTableAdapter;
 
+        UpdateDesignProjectPermitCostEntryTableAdapters.QueriesTableAdapter aUpdateDesignProjectPermitCostTableAdapter;
+
+        DesignPermitImportDataSet aDesignPermitImportDataSet;
+        DesignPermitImportDataSetTableAdapters.designpermitimportTableAdapter aDesignPermitImportTableAdapter;
+
+        InsertDesignPermitImportEntryTableAdapters.QueriesTableAdapter aInsertDesignPermitImportTableAdapter;
+
+        FindDesignPermitImportByTransactionDateDataSet aFindDesignPermitImportbyTransactionDateDataSet;
+        FindDesignPermitImportByTransactionDateDataSetTableAdapters.FindDesignPermitImportByTransactionDateTableAdapter aFindDesignPermitImportByTransactionDateTableAdapter;
+
+        UpdateDesignPermitSubmittedEntryTableAdapters.QueriesTableAdapter aUpdateDesignPermitSubmittedTableAdapter;
+
+        UpdateDesignPermitApprovalEntryTableAdapters.QueriesTableAdapter aUpdateDesignPermitApprovalTableAdapter;
+
+        UpdateDesignPermitExpirationDateEntryTableAdapters.QueriesTableAdapter aUpdateDesignPermitExpirationDateTableAdapter;
+
+        UpdateDesignPermitActivationDateEntryTableAdapters.QueriesTableAdapter aUpdateDesignPermitActivationDateTableAdapter;
+
+        UpdateDesignPermitClosedDateEntryTableAdapters.QueriesTableAdapter aUpdateDesignPermitClosedDateTableAdapter;
+
+        UpdateDesignPermitCommentsEntryTableAdapters.QueriesTableAdapter aUpdateDesignPermitCommentsTableAdapter;
+
+        UpdateDesignProjectActiveEntryTableAdapters.QueriesTableAdapter aUpdateDesignProjectActiveTableAdapter;
+
+        public bool UpdateDesignProjectActive(int intTransactionID, bool blnProjectActive)
+        {
+            bool blnFatalError = false;
+
+            try
+            {
+                aUpdateDesignProjectActiveTableAdapter = new UpdateDesignProjectActiveEntryTableAdapters.QueriesTableAdapter();
+                aUpdateDesignProjectActiveTableAdapter.UpdateDesignProjectActive(intTransactionID, blnProjectActive);
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Design Permit Class // Update Design Project Active " + Ex.Message);
+
+                blnFatalError = true;
+            }
+
+            return blnFatalError;
+        }
+        public bool UpdateDesignPermitComments(int intTransactionID, string strPermitComments)
+        {
+            bool blnFatalError = false;
+
+            try
+            {
+                aUpdateDesignPermitCommentsTableAdapter = new UpdateDesignPermitCommentsEntryTableAdapters.QueriesTableAdapter();
+                aUpdateDesignPermitCommentsTableAdapter.UpdateDesignPermitComments(intTransactionID, strPermitComments);
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Design Permit Class // Update Design Permit Comments " + Ex.Message);
+
+                blnFatalError = true;
+            }
+
+            return blnFatalError;
+        }
+        public bool UpdateDesignPermitClosedDate(int intTransactionID, DateTime datPermitClosedDate)
+        {
+            bool blnFatalError = false;
+
+            try
+            {
+                aUpdateDesignPermitClosedDateTableAdapter = new UpdateDesignPermitClosedDateEntryTableAdapters.QueriesTableAdapter();
+                aUpdateDesignPermitClosedDateTableAdapter.UpdateDesignPermitClosedDate(intTransactionID, datPermitClosedDate);
+            }
+            catch(Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Design Permit Class // Update Design Permit Closed Date " + Ex.Message);
+
+                blnFatalError = true;
+            }
+
+            return blnFatalError;
+        }
+        public bool UpdateDesignPermitActivationDate(int intTransactionID, DateTime datActivationDate)
+        {
+            bool blnFatalError = false;
+
+            try
+            {
+                aUpdateDesignPermitActivationDateTableAdapter = new UpdateDesignPermitActivationDateEntryTableAdapters.QueriesTableAdapter();
+                aUpdateDesignPermitActivationDateTableAdapter.UpdateDesignPermitActivationDate(intTransactionID, datActivationDate);
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Design Permit Class // Update Design Permit Activation Date " + Ex.Message);
+
+                blnFatalError = true;
+            }
+
+            return blnFatalError;
+        }
+        public bool UpdateDesignPermitExpirationDate(int intTransactionID, DateTime datPermitExpiration)
+        {
+            bool blnFatalError = false;
+
+            try
+            {
+                aUpdateDesignPermitExpirationDateTableAdapter = new UpdateDesignPermitExpirationDateEntryTableAdapters.QueriesTableAdapter();
+                aUpdateDesignPermitExpirationDateTableAdapter.UpdateDesignPermitExperationDate(intTransactionID, datPermitExpiration);
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Design Permit Class // Update Design Permit Expiration Date " + Ex.Message);
+
+                blnFatalError = true;
+            }
+
+            return blnFatalError;
+        }
+        public bool UpdateDesignPermitApproval(int intTransactionID, DateTime datPermitApproved)
+        {
+            bool blnFatalError = false;
+
+            try
+            {
+                aUpdateDesignPermitApprovalTableAdapter = new UpdateDesignPermitApprovalEntryTableAdapters.QueriesTableAdapter();
+                aUpdateDesignPermitApprovalTableAdapter.UpdateDesignPermitApproval(intTransactionID, datPermitApproved);
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Design Permit Class // Update Design Permit Approval " + Ex.Message);
+
+                blnFatalError = true;
+            }
+
+            return blnFatalError;
+        }
+        public bool UpdateDesignPermitSubmitted(int intTransactionID, DateTime datPermitSubmitted)
+        {
+            bool blnFatalError = false;
+
+            try
+            {
+                aUpdateDesignPermitSubmittedTableAdapter = new UpdateDesignPermitSubmittedEntryTableAdapters.QueriesTableAdapter();
+                aUpdateDesignPermitSubmittedTableAdapter.UpdateDesignPermitSubmitted(intTransactionID, datPermitSubmitted);
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Design Permit Class // Update Design Permit Submitted " + Ex.Message);
+
+                blnFatalError = true;
+            }
+
+            return blnFatalError;
+        }
+        public FindDesignPermitImportByTransactionDateDataSet FindDesignPermitImportbyTransactionDate(DateTime datTransactionDate)
+        {
+            try
+            {
+                aFindDesignPermitImportbyTransactionDateDataSet = new FindDesignPermitImportByTransactionDateDataSet();
+                aFindDesignPermitImportByTransactionDateTableAdapter = new FindDesignPermitImportByTransactionDateDataSetTableAdapters.FindDesignPermitImportByTransactionDateTableAdapter();
+                aFindDesignPermitImportByTransactionDateTableAdapter.Fill(aFindDesignPermitImportbyTransactionDateDataSet.FindDesignPermitImportByTransactionDate, datTransactionDate);
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Design Permits Class // Find Design Permit Import by Transaction Date " + Ex.Message);
+            }
+
+            return aFindDesignPermitImportbyTransactionDateDataSet;
+        }
+        public bool InsertDesignPermitImport(int intProjectID, string strCarlID, string strSurveyType, string strJobName, string strFieldEngineer, string strConstSupervisor, string strPermitType, DateTime datIssueDate, string strPermitAgency, DateTime datTransactionDate)
+        {
+            bool blnFatalError = false;
+
+            try
+            {
+                aInsertDesignPermitImportTableAdapter = new InsertDesignPermitImportEntryTableAdapters.QueriesTableAdapter();
+                aInsertDesignPermitImportTableAdapter.InsertDesignPermitImport(intProjectID, strCarlID, strSurveyType, strJobName, strFieldEngineer, strConstSupervisor, strPermitType, datIssueDate, strPermitAgency, datTransactionDate);
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Design Permit Class // Insert Design Permit Import " + Ex.Message);
+
+                blnFatalError = true;
+            }
+
+            return blnFatalError;
+        }
+        public DesignPermitImportDataSet GetDesignPermitImportInfo()
+        {
+            try
+            {
+                aDesignPermitImportDataSet = new DesignPermitImportDataSet();
+                aDesignPermitImportTableAdapter = new DesignPermitImportDataSetTableAdapters.designpermitimportTableAdapter();
+                aDesignPermitImportTableAdapter.Fill(aDesignPermitImportDataSet.designpermitimport);
+            }
+            catch(Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Design Permit Class // Get Design Permit Import Info " + Ex.Message);
+            }
+
+            return aDesignPermitImportDataSet;
+        }
+        public void UpdateDesignPermitImportDB(DesignPermitImportDataSet aDesignPermitImportDataSet)
+        {
+            try
+            {
+                aDesignPermitImportTableAdapter = new DesignPermitImportDataSetTableAdapters.designpermitimportTableAdapter();
+                aDesignPermitImportTableAdapter.Update(aDesignPermitImportDataSet.designpermitimport);
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Design Permit Class // Update Design Permit Import DB " + Ex.Message);
+            }
+        }
+        public bool UpdateDesignProjectPermitCost(int intTransactionID, decimal decPermitCost)
+        {
+            bool blnFatalError = false;
+
+            try
+            {
+                aUpdateDesignProjectPermitCostTableAdapter = new UpdateDesignProjectPermitCostEntryTableAdapters.QueriesTableAdapter();
+                aUpdateDesignProjectPermitCostTableAdapter.UpdateDesignProjectPermitCost(intTransactionID, decPermitCost);
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Design Permit Class // Update Design Project Permit Cost " + Ex.Message);
+
+
+                blnFatalError = true;
+            }
+
+            return blnFatalError;
+        }
         public FindPermitsByLocationDataSet FindPermitsByLocation(int intWarehouseID, DateTime datStartDate, DateTime datEndDate)
         {
             try
